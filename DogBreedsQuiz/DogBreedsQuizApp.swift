2234431a8d4cfff6a7dba8e8911233d272dcc26f
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-10-02 13:39:07 -0400

    Initial Commit

---
+++ DogBreedsQuiz/DogBreedsQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsQuizApp.swift
+//  DogBreedsQuiz
+//
+//  Created by Jibryll Brinkley on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
